23672fea694eb82c6ab819abb1291b3656a0a03a
Leon Malisov <leonmalisov@gmail.com>
2020-01-09 19:55:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,18 @@
 #write your code here
 
-def countdown(time)
-  while time > 0
+def countdown(seconds)
+  while seconds > 0
     puts "#{time} SECOND(S)!"
-    time -= 1
+    seconds -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{time} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
   end
   "HAPPY NEW YEAR!"
 end
